3850b68fff426ddd727900bcf939fe663101edb2
Facu Lacarbonaro <faculacarbonaro@gmail.com>
2020-12-02 09:48:41 -0300

    add build

---
+++ build/precache-manifest.5f00263ea320793a913e9bbf218d5c42.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08456c7814b23fe4dbf62e3808b4163e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da2761df8ffd006fd621",
+    "url": "/static/css/main.a75f057f.chunk.css"
+  },
+  {
+    "revision": "18719ffe6a6068084e92",
+    "url": "/static/js/2.7492b9e4.chunk.js"
+  },
+  {
+    "revision": "89a60c54d353d6cd982e142668b3ef72",
+    "url": "/static/js/2.7492b9e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da2761df8ffd006fd621",
+    "url": "/static/js/main.84cf3070.chunk.js"
+  },
+  {
+    "revision": "3cc9d6562f64fc197f20",
+    "url": "/static/js/runtime-main.fc30dc24.js"
+  },
+  {
+    "revision": "51211fd8d3dea7f7307a75412c5898ce",
+    "url": "/static/media/banner-1.51211fd8.jpg"
+  },
+  {
+    "revision": "253688e0e530a75620b5ae3c79ea90f2",
+    "url": "/static/media/banner-2.253688e0.jpg"
+  },
+  {
+    "revision": "a4c4ffb949da53bee9c2a38a90b59481",
+    "url": "/static/media/banner-3.a4c4ffb9.jpg"
+  },
+  {
+    "revision": "c2aaf46180f85f2b318e5208838ac47f",
+    "url": "/static/media/banner-4.c2aaf461.jpg"
+  },
+  {
+    "revision": "1c1a71fbf3bfd86783737a9015391dde",
+    "url": "/static/media/banner-5.1c1a71fb.jpg"
+  },
+  {
+    "revision": "4798cb31374c345752a28e7ab0dd9f5d",
+    "url": "/static/media/logo.4798cb31.png"
+  },
+  {
+    "revision": "c5d0d9b790d7fd5060b2c66af7643135",
+    "url": "/static/media/logo__white.c5d0d9b7.png"
+  },
+  {
+    "revision": "f07f21c1eeb7e252ec02a80f16db1cd6",
+    "url": "/static/media/post-1.f07f21c1.jpg"
+  },
+  {
+    "revision": "e9528e83f23378863209abc44211196a",
+    "url": "/static/media/post-2.e9528e83.jpg"
+  },
+  {
+    "revision": "b2c7e6a5770c99d476c3631272bb2432",
+    "url": "/static/media/post-3.b2c7e6a5.jpg"
+  },
+  {
+    "revision": "686651df0c6cd621955f469bab57e994",
+    "url": "/static/media/post-4.686651df.jpg"
+  }
+]);
